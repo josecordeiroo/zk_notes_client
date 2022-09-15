891c2ba3c0ca2a43b1c98aa195ec740a4a5ffc88
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-14 22:33:53 -0300

    crete home screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Fragment } from "react";
 import "./App.scss";
-import { Notification, Section } from "rbx";
-import Header from "./components/header";
+import Home from "./screens/home";
 
 const App = () => (
   <Fragment>
-    <Header/>
-    <Section>
-      <Notification color="success">Zk Notes</Notification>
-    </Section>
+    <Home/>
   </Fragment>
 );
 
---
+++ src/screens/home/index.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from "react";
+
+import Header from "../../components/header";
+
+import { Column, Section, Title, Container } from "rbx";
+
+import "../../styles/home.scss";
+
+const Home = () => (
+  <Fragment>
+    <Header />
+    <Section size="medium" className="home">
+      <Container>
+        <Column.Group>
+          <Column size={5}>
+            <Title size={2} spaced className="has-text-white">
+              Crie suas notas e tenha sempre a mão
+            </Title>
+            <Title size={5} spaced className="has-text-light" subtitle>
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs.
+              <br />
+              <br />
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print.
+            </Title>
+            <a href="/" className="button is-outlined is-white is-large">
+              <strong>Registre-se gratuitamente</strong>
+            </a>
+          </Column>
+          <Column size={6} offset={1}>
+          <img id="gadgets" src="https://images.pexels.com/photos/4300393/pexels-photo-4300393.png?cs=srgb&dl=pexels-dominika-roseclay-4300393.jpg&fm=jpg" alt=""/>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
+  </Fragment>
+);
+
+export default Home;
