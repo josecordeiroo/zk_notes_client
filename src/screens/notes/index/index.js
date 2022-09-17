e89beedb771a643b2a396ba81f4371d7f59d9334
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-17 17:58:08 -0300

    protect routes

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,5 +1,23 @@
 import React, { Fragment } from "react";
 
-const Notes = () => <Fragment>Hello World</Fragment>;
+import { Navigate } from "react-router-dom";
+
+const Notes = () => {
+  if (!localStorage.getItem("user")) {
+    return (
+      <Navigate
+        to={{
+          pathname: "/login"
+        }}
+      />
+    );
+  }
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+};
 
 export default Notes;
