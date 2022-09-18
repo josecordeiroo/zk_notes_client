2249fbfeaadad15e1c57dd52e6c390a0f1263f3d
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-18 00:13:00 -0300

    create list of notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,12 +1,41 @@
 import { Column } from "rbx";
 import { ColumnGroup } from "rbx/grid/columns/column-group";
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 
 import { push as Menu } from "react-burger-menu";
 
 import "../../styles/notes.scss";
 
+import ListNotes from "./list";
+import NotesServices from "../../services/notes";
+
 const Notes = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState({
+    title: "",
+    body: "",
+    id: "",
+  });
+
+  async function fetchNotes() {
+    const response = await NotesServices.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse());
+      setCurrentNote(response.data[0]);
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    });
+    setCurrentNote(note);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <Fragment className="notesStyle">
       <ColumnGroup>
@@ -24,9 +53,11 @@ const Notes = (props) => {
               Search...
             </Column>
 
-            <Column size={10} offset={1}>
-              List...
-            </Column>
+            <ListNotes
+              notes={notes}
+              selectNote={selectNote}
+              currentNote={currentNote}
+            ></ListNotes>
           </ColumnGroup>
         </Menu>
         <Column size={12} className="notes-editor" id="notes-editor">
---
+++ src/components/notes/list/index.js
@@ -0,0 +1,41 @@
+import { Column, Title, List, Tag } from "rbx";
+import { ColumnGroup } from "rbx/grid/columns/column-group";
+import React, { Fragment } from "react";
+import Moment from "moment";
+
+const ListNotes = (props) => {
+  return (
+    <Fragment>
+      <ColumnGroup breakpoint="mobile">
+        <Column size={6} offset={1}>
+          <Title size={6}>{props.notes.length} notas</Title>
+        </Column>
+      </ColumnGroup>
+      <List>
+        {props.notes.map((item, key) => (
+          <List.Item
+            key={key}
+            onClick={() => props.selectNote(item._id)}
+            active={item == props.currentNote}
+          >
+            <Tag rounded color="light">
+              {Moment(item.created_at).format("DD/MM")}
+            </Tag>
+            <Title size={6}>
+              {item.title.replace(/(<([^>]+)>)/gi, "").substring(0, 15)}
+            </Title>
+            <Title size={6} subtitle spaced={false}>
+              {item.body.replace(/(<([^>]+)>)/gi, "").substring(0, 30)}
+            </Title>
+
+            <ColumnGroup breakpoint="mobile">
+              <Column size={10}></Column>
+            </ColumnGroup>
+          </List.Item>
+        ))}
+      </List>
+    </Fragment>
+  );
+};
+
+export default ListNotes;
